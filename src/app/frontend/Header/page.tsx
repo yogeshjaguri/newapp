14dc441c8320cab7ce2017a947f5fa691c394cd3
Yogesh <yogesh.jaguri2401@gmail.com>
2023-07-18 10:47:06 +0530

    updated

---
+++ src/app/frontend/Header/page.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { AiOutlineShoppingCart } from 'react-icons/ai'
+
+
+const page = () => {
+  return (
+    <div className='h-16 flex bg-white  justify-between'>
+        <input className='border flex-1 border-gray-400 rounded-lg p-2 m-2 max-w-xl' type="text" placeholder='search'/>
+
+        <div className='flex items-center'>
+        <AiOutlineShoppingCart size={30} className='m-2 relative' color='#00ced1' />
+        <p className='absolute right-2 top-3 bg-red-500 text-white font-bold rounded-full px-1 text-xs'>1</p>
+        </div>
+    </div>
+  )
+}
+
+export default page
