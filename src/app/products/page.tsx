06f84effc5c0c932fa6392e05287034f0cc56444
Yogesh <yogesh.jaguri2401@gmail.com>
2023-08-05 16:45:14 +0530

    new updates

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
-import Product from '../product/page'
-import useProduct from '@/hooks/useProduct';
-import useFilter from '@/hooks/useFilter';
-import Sort from '@/components/Sort';
+import Sort from "@/components/Sort";
+import useFilter from "@/hooks/useFilter";
+import useProduct from "@/hooks/useProduct";
+import Product from "../product/page";
+
 
 const Products = () => {
   const {filteredProducts}  = useFilter() as any;
